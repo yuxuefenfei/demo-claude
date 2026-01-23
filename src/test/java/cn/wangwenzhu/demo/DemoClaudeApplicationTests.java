b605ee5eac28f42468c84508b2abbb94e6bf49ef
王文柱 <yuxuefenfei0@gmail.com>
2026-01-23 17:55:15 +0800

    init.

---
+++ src/test/java/cn/wangwenzhu/demo/DemoClaudeApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.wangwenzhu.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoClaudeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
